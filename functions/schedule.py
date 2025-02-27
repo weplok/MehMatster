e466495ccf65de12a1391d5c8874bb308bab9e49
weplok <jess.makaronin228@gmail.com>
2025-02-27 12:33:31 +0300

    get_schedule def

--- functions/schedule.py
+++ functions/schedule.py
@@ -10,14 +10,19 @@
 Студент может получить быстрое расписание по своей группе, не вводя ее каждый раз.
 """
 
-import requests
 import csv
+import datetime as dt
+
+import pandas
+import requests
 
 with open("files/courses.tsv", "r", encoding="utf-8") as csv_file:
     reader = csv.DictReader(csv_file, delimiter="\t")
     course_urls = dict()
     for row in reader:
         course_urls[row["course"]] = row["id"]
+
+groups_urls = pandas.read_csv("files/groups.tsv", delimiter="\t")
 
 
 # Получить список групп по названию курса
@@ -29,3 +34,53 @@ def get_groups(course: str) -> list:
         groups.append(f"{group['name']}, {group['num']} группа")
     return groups
 
+
+def get_schedule(course: str, group: str, period: str):
+    """
+    :param course: Курс полностью (напр: Бакалавриат, 1 курс)
+    :param group: Группа полностью (напр: ПМИ, 1 группа)
+    :param period: За какой период нужно расписание (today - сегодня, tomorrow - завтра, atomorrow - послезавтра, week - неделя)
+    :return:
+    """
+
+    group_url = groups_urls[groups_urls.course == course][groups_urls.group == group].get("id").iat[0]
+    url = f"https://schedule.sfedu.ru/APIv1/schedule/group/{group_url}"
+    response = requests.get(url=url).json()
+
+    lessons = pandas.DataFrame(response["lessons"])
+    lessons["weekday"] = lessons["timeslot"].transform(lambda x: x[1])
+    lessons["start_time"] = lessons["timeslot"].transform(lambda x: x[3:8])
+    lessons["end_time"] = lessons["timeslot"].transform(lambda x: x[12:17])
+    lessons = lessons.sort_values(by=["weekday", "start_time"])
+
+    curricula = pandas.DataFrame(response["curricula"])
+
+    weekday = dt.datetime.today().weekday()
+    if period == "today":
+        pass
+    elif period == "tomorrow":
+        weekday += 1
+    elif period == "atomorrow":
+        weekday += 2
+    elif period == "week":
+        weekday = None
+    if weekday:
+        if weekday > 6:
+            weekday -= 7
+        lessons = lessons[lessons.weekday == str(weekday)]
+
+    schedule = dict()
+    for index, row in lessons.iterrows():
+        curric = curricula[curricula.lessonid == row["id"]]
+        if row["weekday"] not in schedule.keys():
+            schedule[row["weekday"]] = list()
+        for indx, rowc in curric.iterrows():
+            schedule[row["weekday"]].append({
+                "title": rowc["subjectname"],
+                "teacher": rowc["teachername"],
+                "room": rowc["roomname"],
+                "info": row["info"],
+                "start_time": row["start_time"],
+                "end_time": row["end_time"],
+            })
+    return schedule

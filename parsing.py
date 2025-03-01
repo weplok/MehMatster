e51757689372e11d62e2f49ccfb2a0985f78fb1c
verlandiya <VeraGonch@yandex.ru>
2025-03-01 15:07:26 +0300

    csv направления и парсинг магистратуры бакалавриата

--- parsing.py
+++ parsing.py
@@ -58,8 +58,8 @@ def student_get_news_mehmat():
     return "".join(filter_news_cards)
 
 
-# функция возвращает количество бюджетных мест исходя из запроса пользователя
-def applicant_count_free_places(direction):
+#бакалавр три функции получение коммерцион мест бюджет мест и проход баллы
+def bachalor_applicant_count_free_places(direction):
     url = "https://sfedu.ru/www/stat_pages22.show?p=ABT/N8206"
     page = requests.get(url=url)
 
@@ -73,30 +73,22 @@ def applicant_count_free_places(direction):
     fiit_dir_row = soup.find('tr', class_='row4')
     fiit_dir_quantity = fiit_dir_row.find('td', class_='column3 style10 n').text
 
-    moais_dir_row = soup.find('tr', class_='row7')
-    moais_dir_quantity = moais_dir_row.find('td', class_='column3 style10 n').text
+    math_teach_dir_row = soup.find('tr', class_='row90')
+    math_teach_dir_quantity = math_teach_dir_row.find('td', class_='column3 style12 n').text
 
-    pi_dir_row = soup.find('tr', class_='row19')
-    pi_dir_quantity = pi_dir_row.find('td', class_='column3 style10 n').text
-
-    ivt_dir_row = soup.find('tr', class_='row28')
-    ivt_dir_quantity = ivt_dir_row.find('td', class_='column3 style10 n').text
 
     free_places = {
         'прикладная математика и информатика': pmi_dir_quantity,
         'фундаментальная информатика и информационные технологии': fiit_dir_quantity,
-        'информатика и вычислительная техника': ivt_dir_quantity,
         "математика и механика": mim_dir_quantity,
-        'математическое обеспечение и администрирование информационных систем': moais_dir_quantity,
-        'прикладная информатика': pi_dir_quantity
+        'педагогическое образование: математика': math_teach_dir_quantity,
     }
 
     return free_places[direction.lower()]
 
 
 
-# функция возвращает количество платных мест исходя из запроса пользователя
-def applicant_count_paid_places(direction):
+def bachalor_applicant_count_paid_places(direction):
     url = "https://sfedu.ru/www/stat_pages22.show?p=ABT/N8206"
     page = requests.get(url=url)
 
@@ -110,25 +102,21 @@ def applicant_count_paid_places(direction):
     fiit_dir_row = soup.find('tr', class_='row4')
     fiit_dir_quantity = fiit_dir_row.find('td', class_='column7 style10 n').text
 
-    moais_dir_row = soup.find('tr', class_='row7')
-    moais_dir_quantity = moais_dir_row.find('td', class_='column7 style10 n').text
+    math_teach_dir_row = soup.find('tr', class_='row90')
+    math_teach_dir_quantity = math_teach_dir_row.find('td', class_='column7 style10 n').text
 
-    pi_dir_row = soup.find('tr', class_='row19')
-    pi_dir_quantity = pi_dir_row.find('td', class_='column7 style10 n').text
 
-    ivt_dir_row = soup.find('tr', class_='row28')
-    ivt_dir_quantity = ivt_dir_row.find('td', class_='column7 style10 n').text
 
     paid_places = {
         'прикладная математика и информатика': pmi_dir_quantity,
         'фундаментальная информатика и информационные технологии': fiit_dir_quantity,
-        'информатика и вычислительная техника': ivt_dir_quantity,
         "математика и механика": mim_dir_quantity,
-        'математическое обеспечение и администрирование информационных систем': moais_dir_quantity,
-        'прикладная информатика': pi_dir_quantity
+        'педагогическое образование: математика': math_teach_dir_quantity,
     }
 
     return paid_places[direction.lower()]
+
+
 
 def replace_br(el):
     if el.name == 'br':
@@ -136,7 +124,7 @@ def replace_br(el):
     else:
         return el.text
 
-def applicant_pass_balls(direction):
+def bachalor_applicant_pass_balls(direction):
     url = "https://sfedu.ru/www/stat_pages22.show?p=ABT/N8206"
     page = requests.get(url=url)
 
@@ -153,29 +141,175 @@ def applicant_pass_balls(direction):
     fiit_dir_balls = fiit_dir_row.find('td', class_='column8 style20 s')
     fiit_dir_balls = ''.join(map(replace_br, fiit_dir_balls))
 
-
-    moais_dir_row = soup.find('tr', class_='row7')
-    moais_dir_balls = moais_dir_row.find('td', class_='column8 style20 s')
-    moais_dir_balls = ''.join(map(replace_br, moais_dir_balls))
-
-    pi_dir_row = soup.find('tr', class_='row19')
-    pi_dir_balls = pi_dir_row.find('td', class_='column8 style20 s')
-    pi_dir_balls = ''.join(map(replace_br, pi_dir_balls))
-
-    ivt_dir_row = soup.find('tr', class_='row28')
-    ivt_dir_balls = ivt_dir_row.find('td', class_='column8 style20 s')
-    ivt_dir_balls = ''.join(map(replace_br, ivt_dir_balls))
+    math_teach_dir_row = soup.find('tr', class_='row90')
+    math_teach_dir_balls = math_teach_dir_row.find('td', class_='column8 style20 s')
+    math_teach_dir_balls = ''.join(map(replace_br, math_teach_dir_balls))
 
     pass_balls = {
         'прикладная математика и информатика': pmi_dir_balls,
         'фундаментальная информатика и информационные технологии': fiit_dir_balls,
-        'информатика и вычислительная техника': ivt_dir_balls,
         "математика и механика": mim_dir_balls,
-        'математическое обеспечение и администрирование информационных систем': moais_dir_balls,
-        'прикладная информатика': pi_dir_balls
+        'педагогическое образование: математика': math_teach_dir_balls,
     }
 
     return pass_balls[direction.lower()]
+
+
+
+
+#магистратура три функции получение коммерцион мест бюджет мест и проход баллы
+def master_applicant_count_free_places(direction):
+    url = "https://sfedu.ru/www/stat_pages22.show?p=ABT/N8207/P"
+    page = requests.get(url=url)
+
+    soup = BeautifulSoup(page.text, "html.parser")
+    fiit_dir_row = soup.find('tr', class_='row2')
+    fiit_dir_quantity = fiit_dir_row.find('td', class_='column3 style9 n').text
+
+    cm_dir_row = soup.find('tr', class_='row3')
+    cm_dir_quantity = cm_dir_row.find('td', class_='column3 style9 n').text
+
+    msd_dir_row = soup.find('tr', class_='row4')
+    msd_dir_quantity = msd_dir_row.find('td', class_='column3 style9 n').text
+
+    ii_dir_row = soup.find('tr', class_='row5')
+    ii_dir_quantity = ii_dir_row.find('td', class_='column3 style9 n').text
+
+    mitou_dir_row = soup.find('tr', class_='row6')
+    mitou_dir_quantity = mitou_dir_row.find('td', class_='column3 style9 n').text
+
+    rmp_dir_row = soup.find('tr', class_='row9')
+    rmp_dir_quantity = rmp_dir_row.find('td', class_='column3 style9 n').text
+
+    irpi_dir_row = soup.find('tr', class_='row10')
+    irpi_dir_quantity = irpi_dir_row.find('td', class_='column3 style9 n').text
+
+    mio_dir_row = soup.find('tr', class_='row124')
+    mio_dir_quantity = mio_dir_row.find('td', class_='column3 style14 n').text
+
+
+
+    free_places = {
+        'фундаментальная математика, механика и математическое моделирование': fiit_dir_quantity,
+        'computational modeling in technology and finance': cm_dir_quantity,
+        "modern software development": msd_dir_quantity,
+        "разработка мобильных приложений и компьютерных игр": rmp_dir_quantity,
+        'искусственный интеллект: математические модели и прикладные решения': ii_dir_quantity,
+        'модели и информационные технологии организационного управления': mitou_dir_quantity,
+        'информатика, робототехника и прикладные исследования в области информационных технологий': irpi_dir_quantity,
+        'математика и информатика в образовании': mio_dir_quantity
+
+    }
+
+    return free_places[direction.lower()]
+
+
+print(master_applicant_count_free_places('математика и информатика в образовании'))
+
+
+def master_applicant_count_paid_places(direction):
+    url = "https://sfedu.ru/www/stat_pages22.show?p=ABT/N8207/P"
+    page = requests.get(url=url)
+
+    soup = BeautifulSoup(page.text, "html.parser")
+    fiit_dir_row = soup.find('tr', class_='row2')
+    fiit_dir_quantity = fiit_dir_row.find('td', class_='column5 style11 n').text
+
+    cm_dir_row = soup.find('tr', class_='row3')
+    cm_dir_quantity = cm_dir_row.find('td', class_='column5 style11 n').text
+
+    msd_dir_row = soup.find('tr', class_='row4')
+    msd_dir_quantity = msd_dir_row.find('td', class_='column5 style11 n').text
+
+    ii_dir_row = soup.find('tr', class_='row5')
+    ii_dir_quantity = ii_dir_row.find('td', class_='column5 style11 n').text
+
+    mitou_dir_row = soup.find('tr', class_='row6')
+    mitou_dir_quantity = mitou_dir_row.find('td', class_='column5 style11 n').text
+
+    rmp_dir_row = soup.find('tr', class_='row9')
+    rmp_dir_quantity = rmp_dir_row.find('td', class_='column5 style11 n').text
+
+    irpi_dir_row = soup.find('tr', class_='row10')
+    irpi_dir_quantity = irpi_dir_row.find('td', class_='column5 style11 n').text
+
+    mio_dir_row = soup.find('tr', class_='row124')
+    mio_dir_quantity = mio_dir_row.find('td', class_='column5 style11 n').text
+
+
+
+    paid_places = {
+        'фундаментальная математика, механика и математическое моделирование': fiit_dir_quantity,
+        'computational modeling in technology and finance': cm_dir_quantity,
+        "modern software development": msd_dir_quantity,
+        "разработка мобильных приложений и компьютерных игр": rmp_dir_quantity,
+        'искусственный интеллект: математические модели и прикладные решения': ii_dir_quantity,
+        'модели и информационные технологии организационного управления': mitou_dir_quantity,
+        'информатика, робототехника и прикладные исследования в области информационных технологий': irpi_dir_quantity,
+        'математика и информатика в образовании': mio_dir_quantity
+
+    }
+
+    return paid_places[direction.lower()]
+
+
+
+def master_applicant_pass_balls(direction):
+    url = "https://sfedu.ru/www/stat_pages22.show?p=ABT/N8207/P"
+    page = requests.get(url=url)
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    fiit_dir_row = soup.find('tr', class_='row2')
+    fiit_dir_balls = fiit_dir_row.find('td', class_='column6 style8 s').text
+
+    cm_dir_row = soup.find('tr', class_='row3')
+    cm_dir_balls = cm_dir_row.find('td', class_='column6 style8 s').text
+
+    msd_dir_row = soup.find('tr', class_='row4')
+    msd_dir_balls = msd_dir_row.find('td', class_='column6 style8 s').text
+
+    mitou_dir_row = soup.find('tr', class_='row6')
+    mitou_dir_balls = mitou_dir_row.find('td', class_='column6 style8 s').text
+
+    rmp_dir_row = soup.find('tr', class_='row9')
+    rmp_dir_balls = rmp_dir_row.find('td', class_='column6 style8 s').text
+
+    ii_dir_row = soup.find('tr', class_='row5')
+    ii_dir_balls = ii_dir_row.find('td', class_='column6 style8 s').text
+
+    irpi_dir_row = soup.find('tr', class_='row10')
+    irpi_dir_balls = irpi_dir_row.find('td', class_='column6 style8 s').text
+
+    mio_dir_row = soup.find('tr', class_='row124')
+    mio_dir_balls = mio_dir_row.find('td', class_='column6 style8 s').text
+
+
+    pass_balls = {
+        'фундаментальная математика, механика и математическое моделирование': fiit_dir_balls,
+        'computational modeling in technology and finance': cm_dir_balls,
+        "modern software development": msd_dir_balls,
+        "разработка мобильных приложений и компьютерных игр": rmp_dir_balls,
+        'искусственный интеллект: математические модели и прикладные решения': ii_dir_balls,
+        'модели и информационные технологии организационного управления': mitou_dir_balls,
+        'информатика, робототехника и прикладные исследования в области информационных технологий': irpi_dir_balls,
+        'математика и информатика в образовании': mio_dir_balls
+
+    }
+
+    return pass_balls[direction.lower()]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
